c677a8dc3aaf1cfe15229e477b3b624e22b5652c
Roland Leißa <leissa@cs.uni-saarland.de>
2012-07-27 18:44:06 +0200

    added license/file description header

--- include/clang/Sema/SemaSierra.h
+++ include/clang/Sema/SemaSierra.h
@@ -1,3 +1,16 @@
+//===--- SemaSierra.h - Semantic Analysis Helpers for Sierra ----*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides the interface for Sierra-related semantic analysis.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_SEMA_SIERRA_SOURCE_H
 #define LLVM_CLANG_SEMA_SIERRA_SOURCE_H
 
--- lib/Sema/SemaSierra.cpp
+++ lib/Sema/SemaSierra.cpp
@@ -1,3 +1,16 @@
+//===--- SemaSierra.cpp - Semantic Analysis Helpers for Sierra ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file implements Sierra-related semantic analysis.
+//
+//===----------------------------------------------------------------------===//
+
 #include "clang/Sema/Sema.h"
 #include "clang/Sema/Scope.h"
 #include "clang/Sema/SemaSierra.h"
