a876cc3c35111532f3e434ec08d77fb9e24ea5df
Roland Leißa <leissa@cs.uni-saarland.de>
2014-11-27 14:02:43 +0100

    added missing switch-cases for SierraVector

--- lib/CodeGen/ItaniumCXXABI.cpp
+++ lib/CodeGen/ItaniumCXXABI.cpp
@@ -2823,6 +2823,7 @@ void ItaniumRTTIBuilder::BuildVTablePointer(const Type *Ty) {
   // GCC treats vector and complex types as fundamental types.
   case Type::Vector:
   case Type::ExtVector:
+  case Type::SierraVector:
   case Type::Complex:
   case Type::Atomic:
   // FIXME: GCC treats block pointers as fundamental types?!
@@ -3033,6 +3034,7 @@ llvm::Constant *ItaniumRTTIBuilder::BuildTypeInfo(QualType Ty, bool Force,
   case Type::Builtin:
   case Type::Vector:
   case Type::ExtVector:
+  case Type::SierraVector:
   case Type::Complex:
   case Type::BlockPointer:
     // Itanium C++ ABI 2.9.5p4:
